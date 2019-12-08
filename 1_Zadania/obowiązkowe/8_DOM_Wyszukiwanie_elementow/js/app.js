9be96ba3fbd7978cb888b190255241883eeb088f
Brdej Damian <damian.brdej@microsoft.wsei.edu.pl>
2019-12-08 12:08:15 +0100

    task 2 DOM

--- 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
+++ 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
@@ -10,4 +10,17 @@ document.addEventListener("DOMContentLoaded", function () {
 
     getDataAnimation(title[0]);
 
+
+    //zadanie 2
+    
+    const home = document.getElementById("home");
+    const home2 = document.querySelector("#home");
+    console.log(home);
+    console.log(home2);
+
+    const li = document.getElementsByTagName("li")[0];
+    console.log(li);
+
+    const block = document.getElementsByClassName("block")[0];
+    console.log(block);
 });
